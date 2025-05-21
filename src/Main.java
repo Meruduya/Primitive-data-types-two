f9d67882ac8bc32aa71744a6194417db965d4019
Veronika <korynaitroyd@gmail.com>
2025-05-21 17:27:52 +0300

    Change in fail Main

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
 
-        int x = 100; // колличество рублей на счете
-        int y = 1500; // сумма пополнения
+        int balance = 100; // колличество рублей на счете
+        int topUp = 1500; // сумма пополнения
         int bonus;
 
-        if (y >= 1000) {
-            bonus = y / 100;
+        if (topUp >= 1000) {
+            bonus = topUp / 100;
         } else {
             bonus = 0;
         }
-        int total = x + y + bonus;
+        int total = balance + topUp + bonus;
         System.out.println("Итоговый счет: " + total);
     }
 }
